c41ef02c275cc6d1eab3832d3e8cb5c544bfa0c2
Marco Fiordi <marco.fiordi97@gmail.com>
2026-04-14 15:38:00 +0200

    aggiunto stampa dei nomi con il ciclo for

--- ex-js-array-methods/snack-1/script.js
+++ ex-js-array-methods/snack-1/script.js
@@ -2,3 +2,9 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+for (let i = 0; i < names.length; i++){
+    let currentName = names[i];
+    console.log(currentName);
+    
+}
+
